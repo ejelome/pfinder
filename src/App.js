1175b1ec1d4bf530c861007c17e5dd9c6dd40237
ejelome <ejelome@gmail.com>
2020-08-12 23:20:55 +0200

    Add defaultProps

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import React, { Component } from "react";
 import "./App.css";
 
 class Heading extends Component {
+  static defaultProps = {
+    title: "hello, world",
+  };
+
   render() {
     return <h1>{this.props.title}</h1>;
   }
 }
 
 class App extends Component {
-  say(greeting = "hi") {
-    return `${greeting}, world`;
-  }
-
   render() {
-    const greeting = "hello";
-    const title = this.say(greeting).toUpperCase();
-
     return (
       <div className="App">
-        <Heading title={title} />
+        <Heading />
       </div>
     );
   }
