2bb9ef1cbc5b033970ecfa4a3fd9c9056e1222b1
ejelome <ejelome@gmail.com>
2020-08-12 23:00:07 +0200

    Add JSX expression

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  say(greeting = "hi") {
+    return `${greeting}, world`;
+  }
+
   render() {
+    const greeting = "hello";
+
     return (
       <div className="App">
-        <h1>hello, world</h1>
+        <h1>{this.say(greeting).toUpperCase()}</h1>
       </div>
     );
   }
