ac8be1cfd436dcb2834fe947e22d9405809584c5
ejelome <ejelome@gmail.com>
2020-08-12 22:47:41 +0200

    Change component wrapper to fragment (shorthand)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <>
         <h1>hello, world</h1>
-      </Fragment>
+      </>
     );
   }
 }
