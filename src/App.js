c7c76896e210f1f5a22da240e94237eec4f824e6
ejelome <ejelome@gmail.com>
2020-08-12 22:47:07 +0200

    Change component wrapper to fragment (verbose)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Fragment>
         <h1>hello, world</h1>
-      </div>
+      </Fragment>
     );
   }
 }
