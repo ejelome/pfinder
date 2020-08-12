bb1638f64f437141ba318661e2a72fb16553f674
ejelome <ejelome@gmail.com>
2020-08-12 22:48:36 +0200

    Change component wrapper back to div

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <>
+      <div className="App">
         <h1>hello, world</h1>
-      </>
+      </div>
     );
   }
 }
