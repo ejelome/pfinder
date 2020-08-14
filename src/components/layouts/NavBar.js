bd6ecb4de10220ce6533160b2abbb7f038be754d
ejelome <ejelome@gmail.com>
2020-08-14 21:46:31 +0200

    Convert NavBar component from class to function

--- src/components/layouts/NavBar.js
+++ src/components/layouts/NavBar.js
@@ -1,33 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-class NavBar extends Component {
-  state = {};
+const NavBar = ({ links }) => {
+  return (
+    <nav className="nav-bar">
+      <ul>
+        {links.map(({ title, url }, i) => (
+          <li key={i}>
+            <Link to={url}>{title}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-  static defaultProps = {
-    links: [{ title: "Home", url: "/" }],
-  };
+NavBar.defaultProps = {
+  links: [{ title: "Home", url: "/" }],
+};
 
-  static propTypes = {
-    links: PropTypes.array.isRequired,
-  };
-
-  render() {
-    const { links } = this.props;
-
-    return (
-      <nav className="nav-bar">
-        <ul>
-          {links.map(({ title, url }, i) => (
-            <li key={i}>
-              <Link to={url}>{title}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    );
-  }
-}
+NavBar.propTypes = {
+  links: PropTypes.array.isRequired,
+};
 
 export default NavBar;
