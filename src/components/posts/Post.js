a1d011d410b5575bc26114f06a6d21fefab1234a
ejelome <ejelome@gmail.com>
2020-08-14 21:30:44 +0200

    Convert Post component from class to function with useEffect

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,39 +1,33 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 
-class Post extends Component {
-  state = {};
-
-  static defaultProps = {};
-
-  static propTypes = {
-    getPost: PropTypes.func.isRequired,
-    post: PropTypes.object.isRequired,
-    match: PropTypes.shape({
-      params: PropTypes.shape({
-        id: PropTypes.number.isRequired,
-      }),
-    }),
-  };
-
-  componentDidMount() {
-    const { getPost } = this.props;
-    const { id } = this.props.match.params;
+const Post = ({ getPost, post, match }) => {
+  useEffect(() => {
+    const { id } = match.params;
 
     getPost(id);
-  }
+  });
 
-  render() {
-    const { post } = this.props;
-    const { title, body } = post;
+  const { title, body } = post;
 
-    return (
-      <div className="post">
-        <h1>{title}</h1>
-        <p>{body}</p>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="post">
+      <h1>{title}</h1>
+      <p>{body}</p>
+    </div>
+  );
+};
+
+Post.defaultProps = {};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  }),
+};
 
 export default Post;
